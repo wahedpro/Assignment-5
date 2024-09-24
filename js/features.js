1a11581eb02164e05342e518ec713879132584ed
Wahedul Islam <wahedulislam.pro@gmail.com>
2024-09-24 07:07:12 +0600

    added some functionality

---
+++ js/features.js
@@ -0,0 +1,7 @@
+document.getElementById('donation-btn').addEventListener('click', function(){
+    showSection('card-section');
+})
+
+document.getElementById('history-btn').addEventListener('click', function(){
+    showSection('history-section');
+})
--- js/utilities.js
+++ js/utilities.js
@@ -5,11 +5,16 @@ function findBalance(id){
     return balanceIs;
 }
 
-// find input fill
-function findInputFill(id){
+function findInputFill(id) {
     const addMoney = document.getElementById(id).value;
-    if(!isNaN(addMoney) && parseFloat(addMoney) > 0){
+    if (!isNaN(addMoney) && parseFloat(addMoney) > 0) {
         const MoneyIs = parseFloat(addMoney);
+        const newBalance = MoneyIs; 
+        // Create and append the h1 element
+        const div = document.createElement('div');
+        div.classList.add('p-6', 'border-2', 'border-gray-100', 'rounded-xl',);
+        div.innerHTML = `<h1 class="font-bold"> ${newBalance} Taka is </h1>`;
+        document.getElementById('history-list').appendChild(div);
         return MoneyIs;
     } else {
         alert("This is Invalid Input");
@@ -28,3 +33,16 @@ function additionCardBalance(id, a, b){
     const newBalance = a + b;
     document.getElementById(id).innerText = newBalance;
 }
+
+// show the section or page
+function showSection(id){
+    document.getElementById('card-section').classList.add('hidden');
+    document.getElementById('history-section').classList.add('hidden');
+    document.getElementById(id).classList.remove('hidden');
+}
+
+
+ // some work
+ const h1 = document.createElement('h1');
+ h1.innerText = `${newBalance} Taka is Donated for famine-2024 at Feni, Bangladesh`;
+ document.getElementById('history-list').appendChild(h1);
